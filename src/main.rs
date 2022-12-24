7e5f5a64bd2a989398445825dcd540616a94ce79
Kyuuhachi <caagr98@gmail.com>
2022-12-24 20:20:05 +0100

    Start working

---
+++ src/main.rs
@@ -0,0 +1,307 @@
+use std::path::{Path, PathBuf};
+use std::collections::HashMap;
+
+use themelios::gamedata::GameData;
+use themelios::scena;
+use themelios::scena::code::{Expr, FlatInsn, Insn, InsnArgMut as IAM};
+use themelios::scena::code::decompile::{TreeInsn, decompile, recompile};
+use themelios::scena::ed7::Scena;
+use themelios::tables::quest::{ED7Quest, self};
+use themelios::text::Text;
+use themelios::types::{QuestId, Flag};
+use visit::VisitMut;
+
+mod visit;
+
+macro_rules! f {
+	($p:pat => $v:expr) => { |_a| {
+		#[allow(irrefutable_let_patterns)]
+		if let $p = _a { Some($v) } else { None }
+	} };
+	($p:pat) => { |_a| {
+		matches!(_a, $p)
+	} };
+}
+
+trait Translator {
+	fn comment(&mut self, s: &str);
+	fn translate(&mut self, s: &str) -> String;
+}
+
+struct Dump {}
+impl Translator for Dump {
+	fn comment(&mut self, s: &str) {
+		println!("## {s}");
+	}
+
+	fn translate(&mut self, s: &str) -> String {
+		println!("{s:?}");
+		s.to_owned()
+	}
+}
+
+struct Nil;
+impl Translator for Nil {
+	fn comment(&mut self, _: &str) {}
+
+	fn translate(&mut self, s: &str) -> String {
+		panic!("no translation expected! {s}");
+	}
+}
+
+struct Context {
+	psp_path:   PathBuf,
+	evo_path:   PathBuf,
+	gf_path:    PathBuf,
+	scenas:     HashMap<String, AScena>,
+	quests:     Vec<ED7Quest>,
+	evo_quests: Vec<ED7Quest>,
+}
+
+impl Context {
+	fn new(
+		psp: impl AsRef<Path>,
+		evo: impl AsRef<Path>,
+		gf: impl AsRef<Path>,
+		quests: impl AsRef<Path>,
+		evo_quests: impl AsRef<Path>,
+	) -> Context {
+		Context {
+			psp_path: psp.as_ref().to_owned(),
+			evo_path: evo.as_ref().to_owned(),
+			gf_path: gf.as_ref().to_owned(),
+			scenas: HashMap::new(),
+			quests: quest::read_ed7(GameData::AO, &std::fs::read(quests).unwrap()).unwrap(),
+			evo_quests: quest::read_ed7(GameData::AO_EVO, &std::fs::read(evo_quests).unwrap()).unwrap(),
+		}
+	}
+
+	fn scena(&mut self, name: &str) -> &mut AScena {
+		self.scenas.entry(name.to_owned()).or_insert_with(|| {
+			let mut gf = scena::ed7::read(GameData::AO, &std::fs::read(self.gf_path.join(format!("{name}.bin"))).unwrap()).unwrap();
+			let psp = scena::ed7::read(GameData::AO, &std::fs::read(self.psp_path.join(format!("{name}.bin"))).unwrap()).unwrap();
+			let evo = scena::ed7::read(GameData::AO_EVO, &std::fs::read(self.evo_path.join(format!("{name}.bin"))).unwrap()).unwrap();
+			assert_eq!(gf.functions.len(), psp.functions.len());
+			for (a, b) in gf.functions.iter_mut().zip(psp.functions.iter()) {
+				if let Some(c) = merge_gf(a, b) {
+					*a = c;
+				}
+			}
+			AScena {
+				main: gf,
+				evo,
+				new_npcs: Vec::new(),
+				new_lps: Vec::new(),
+				new_funcs: Vec::new(),
+			}
+		})
+	}
+
+	fn copy_scena(&mut self, name: &str, tl: &mut impl Translator) -> &mut AScena {
+		todo!("copy a whole scena, usually a _1, wholesale")
+	}
+
+	fn copy_quest(&mut self, id: QuestId, tl: &mut impl Translator) -> &mut ED7Quest {
+		let q1 = self.quests.iter_mut().find(|a| a.id == id).unwrap();
+		let q2 = self.evo_quests.iter().find(|a| a.id == id).unwrap();
+		*q1 = translate(tl, q2);
+		q1
+	}
+}
+
+fn merge_gf(gf: &[FlatInsn], psp: &[FlatInsn]) -> Option<Vec<FlatInsn>> {
+	let mut gf = gf.to_owned();
+	let mut psp = psp.to_owned(); // Because I don't have any non-mut visit
+	enum I {
+		Text(Text),
+		TextTitle(String),
+		MenuItem(String),
+	}
+	let mut texts = Vec::new();
+	gf.accept_mut(&mut |a| match a {
+		IAM::Text(a) => texts.push(I::Text(a.clone())),
+		IAM::TextTitle(a) => texts.push(I::TextTitle(a.clone())),
+		IAM::MenuItem(a) => texts.push(I::MenuItem(a.clone())),
+		_ => {}
+	});
+	texts.reverse();
+	let mut success = true;
+	psp.accept_mut(&mut |a| match (a, texts.pop()) {
+		(IAM::Text(a), Some(I::Text(b))) => *a = b,
+		(IAM::TextTitle(a), Some(I::TextTitle(b))) => *a = b,
+		(IAM::MenuItem(a), Some(I::MenuItem(b))) => *a = b,
+		(IAM::Text(..) | IAM::TextTitle(..) | IAM::MenuItem(..), _) => success = false,
+		_ => {}
+	});
+	success &= texts.is_empty();
+	success.then_some(psp)
+}
+
+struct AScena {
+	main: Scena,
+	evo: Scena,
+
+	new_npcs: Vec<usize>,
+	new_lps: Vec<usize>,
+	new_funcs: Vec<usize>,
+}
+
+impl AScena {
+	fn remap(&mut self, v: &mut impl FnMut(IAM)) {
+		self.evo.accept_mut(v);
+		for i in &self.new_npcs {
+			self.main.npcs[*i].accept_mut(v);
+		}
+		for i in &self.new_lps {
+			self.main.look_points[*i].accept_mut(v);
+		}
+		for i in &self.new_funcs {
+			self.main.functions[*i].accept_mut(v);
+		}
+	}
+
+	fn copy_npc(&mut self, idx: usize, tl: &mut impl Translator) {
+		let monster_start = (8+self.main.npcs.len()) as u16;
+		let monster_end = (8+self.main.npcs.len()+self.main.monsters.len()) as u16;
+		self.main.accept_mut(&mut |a| {
+			if let IAM::CharId(a) = a {
+				if a.0 >= monster_start && a.0 < monster_end {
+					a.0 += 1;
+				}
+			}
+		});
+
+		let new_idx = self.main.npcs.len();
+
+		let start = 8 + idx as u16;
+		let end = 8 + new_idx as u16;
+		self.remap(&mut |a| {
+			if let IAM::CharId(a) = a {
+				if a.0 == start {
+					a.0 = end;
+				} else if start < a.0 && a.0 <= end {
+					a.0 -= 1;
+				}
+			}
+		});
+
+		let npc = self.evo.npcs.remove(idx);
+		self.main.npcs.insert(new_idx, translate(tl, &npc));
+		self.evo.npcs.insert(new_idx, npc);
+		self.new_npcs.push(new_idx);
+	}
+
+	fn copy_func(&mut self, scp: u16, idx: usize, tl: &mut impl Translator) {
+		let new_idx = self.main.functions.len();
+
+		let start = idx as u16;
+		let end = new_idx as u16;
+		self.remap(&mut |a| {
+			if let IAM::FuncRef(a) = a {
+				if a.0 == scp {
+					if a.1 == start {
+						a.1 = end;
+					} else if start < a.1 && a.1 <= end {
+						a.1 -= 1;
+					}
+				}
+			}
+		});
+
+		let func = self.evo.functions.remove(idx);
+		self.main.functions.insert(new_idx, translate(tl, &func));
+		self.evo.functions.insert(new_idx, func);
+		self.new_funcs.push(new_idx);
+	}
+
+	fn copy_look_point(&mut self, idx: usize) -> usize {
+		todo!();
+	}
+
+	fn func(&mut self, idx: usize, f: impl FnOnce(&mut AList<Vec<TreeInsn>>)) {
+		let mut f1 = decompile(&self.main.functions[idx]).unwrap();
+		let f2 = decompile(&self.evo.functions[idx]).unwrap();
+		f(&mut AList { main: &mut f1, evo: &f2 });
+		self.main.functions[idx] = recompile(&f1).unwrap();
+	}
+}
+
+struct AList<'a, T> {
+	main: &'a mut T,
+	evo: &'a T,
+}
+
+impl AList<'_, Vec<TreeInsn>> {
+	fn ifs(&mut self, n: usize) -> AList<Vec<(Option<Expr>, Vec<TreeInsn>)>> {
+		AList {
+			main: self.main.iter_mut().filter_map(f!(TreeInsn::If(x) => x)).nth(n).unwrap(),
+			evo:  self.evo     .iter().filter_map(f!(TreeInsn::If(x) => x)).nth(n).unwrap(),
+		}
+	}
+}
+
+impl<A: PartialEq, B> AList<'_, Vec<(A, B)>> {
+	fn clause(&mut self, k: &A) -> AList<B> {
+		AList {
+			main: self.main.iter_mut().find_map(|(a,b)| (a == k).then_some(b)).unwrap(),
+			evo:  self.evo     .iter().find_map(|(a,b)| (a == k).then_some(b)).unwrap(),
+		}
+	}
+}
+
+impl<T: Clone + VisitMut> AList<'_, Vec<T>> {
+	fn tail(&mut self, tl: &mut impl Translator) {
+		self.main.extend(self.evo[self.main.len()..].iter().map(|a| translate(tl, a)))
+	}
+}
+
+fn translate<T: Clone + VisitMut>(tl: &mut impl Translator, a: &T) -> T {
+	let mut a = a.clone();
+	a.accept_mut(&mut |a| {
+		match a {
+			IAM::Text(a) => *a = translate_text(tl, a),
+			IAM::TextTitle(a) => *a = tl.translate(a),
+			IAM::MenuItem(a) => *a = tl.translate(a),
+			_ => {}
+		}
+	});
+	a
+}
+
+fn translate_text(tl: &mut impl Translator, t: &Text) -> Text {
+	t.clone()
+}
+
+macro_rules! flag {
+	($n:literal) => { Expr::Flag(Flag($n)) }
+}
+
+fn main() {
+	let mut ctx = Context::new(
+		"./data/ao-psp/PSP_GAME/USRDIR/data/scena/",
+		"./data/vita/extract/ao/data1/data/scena/",
+		"./data/ao-gf/data_en/scena/",
+		"./data/ao-gf/data_en/text/t_quest._dt",
+		"./data/vita/extract/ao/data/data/text/t_quest._dt",
+	);
+	let nil = &mut Nil;
+
+	let tl = &mut Dump {};
+	tl.comment("quest");
+	ctx.copy_quest(QuestId(125), tl);
+
+	tl.comment("c1300");
+	let c1300 = ctx.scena("c1300");
+	c1300.main.chcp.push(Some("chr/ch06000.itc".to_owned()));
+	c1300.copy_npc(1, tl);  // Grace
+	c1300.copy_npc(10, tl); // Shirley
+	c1300.copy_npc(11, tl); // Sigmund
+	c1300.copy_func(0, 9, tl); // talk Grace
+	c1300.func(1, |a| a.ifs(1).clause(&Some(flag![2564])).tail(nil));
+
+	tl.comment("c1200");
+	let c1200 = ctx.scena("c1200");
+	c1200.main.chcp[19] = Some("chr/ch28100.itc".to_owned());
+	c1200.copy_npc(31, tl); // Reins
+	c1200.copy_func(0, 107, tl); // talk Reins
+}
---
+++ src/visit.rs
@@ -0,0 +1,196 @@
+use themelios::scena::code::decompile::TreeInsn;
+use themelios::scena::code::{FlatInsn, Expr, Insn, InsnArgMut as IAM};
+use themelios::scena::ed7;
+use themelios::tables::quest::ED7Quest;
+
+pub trait VisitMut {
+	fn accept_mut(&mut self, v: &mut impl FnMut(IAM));
+}
+
+impl<T: VisitMut> VisitMut for Option<T> {
+	fn accept_mut(&mut self, v: &mut impl FnMut(IAM)) {
+		if let Some(a) = self {
+			a.accept_mut(v)
+		}
+	}
+}
+
+impl<T: VisitMut> VisitMut for Vec<T> {
+	fn accept_mut(&mut self, v: &mut impl FnMut(IAM)) {
+		for a in self {
+			a.accept_mut(v)
+		}
+	}
+}
+
+impl VisitMut for ed7::Entry {
+	fn accept_mut(&mut self, v: &mut impl FnMut(IAM)) {
+		accept_val_mut(IAM::FuncRef(&mut self.init), v);
+		accept_val_mut(IAM::FuncRef(&mut self.reinit), v);
+	}
+}
+
+impl VisitMut for ed7::Npc {
+	fn accept_mut(&mut self, v: &mut impl FnMut(IAM)) {
+		accept_val_mut(IAM::TextTitle(&mut self.name), v);
+		accept_val_mut(IAM::FuncRef(&mut self.init), v);
+		accept_val_mut(IAM::FuncRef(&mut self.talk), v);
+	}
+}
+
+impl VisitMut for ed7::Monster {
+	fn accept_mut(&mut self, v: &mut impl FnMut(IAM)) {
+		accept_val_mut(IAM::BattleId(&mut self.battle), v);
+	}
+}
+
+impl VisitMut for ed7::Trigger {
+	fn accept_mut(&mut self, v: &mut impl FnMut(IAM)) {
+		accept_val_mut(IAM::FuncRef(&mut self.function), v);
+	}
+}
+
+impl VisitMut for ed7::LookPoint {
+	fn accept_mut(&mut self, v: &mut impl FnMut(IAM)) {
+		accept_val_mut(IAM::FuncRef(&mut self.function), v);
+	}
+}
+
+impl VisitMut for ed7::Scena {
+	fn accept_mut(&mut self, v: &mut impl FnMut(IAM)) {
+		self.entry.accept_mut(v);
+		self.npcs.accept_mut(v);
+		self.monsters.accept_mut(v);
+		self.triggers.accept_mut(v);
+		self.look_points.accept_mut(v);
+		self.functions.accept_mut(v);
+	}
+}
+
+impl VisitMut for FlatInsn {
+	fn accept_mut(&mut self, v: &mut impl FnMut(IAM)) {
+		match self {
+			FlatInsn::Unless(e, _) => e.accept_mut(v),
+			FlatInsn::Goto(_) => {},
+			FlatInsn::Switch(e, _, _) => e.accept_mut(v),
+			FlatInsn::Insn(i) => i.accept_mut(v),
+			FlatInsn::Label(_) => {},
+		}
+	}
+}
+
+impl VisitMut for TreeInsn {
+	fn accept_mut(&mut self, v: &mut impl FnMut(IAM)) {
+		match self {
+			TreeInsn::If(cs) => {
+				for (e, b) in cs {
+					if let Some(e) = e {
+						e.accept_mut(v);
+					}
+					b.accept_mut(v);
+				}
+			},
+			TreeInsn::Switch(e, cs) => {
+				e.accept_mut(v);
+				for (_, b) in cs {
+					b.accept_mut(v);
+				}
+			},
+			TreeInsn::While(e, b) => {
+				e.accept_mut(v);
+				b.accept_mut(v);
+			},
+			TreeInsn::Break => {}
+			TreeInsn::Continue => {}
+			TreeInsn::Insn(i) => i.accept_mut(v)
+		}
+	}
+}
+
+impl VisitMut for Insn {
+	fn accept_mut(&mut self, v: &mut impl FnMut(IAM)) {
+		for a in Vec::from(self.args_mut()) {
+			accept_val_mut(a, v);
+		}
+	}
+}
+
+impl VisitMut for Expr {
+	fn accept_mut(&mut self, v: &mut impl FnMut(IAM)) {
+		// TODO add type inference somehow
+		accept_val_mut(IAM::Expr(self), v);
+		match self {
+			Expr::Binop(_, l, r) => {
+				l.accept_mut(v);
+				r.accept_mut(v);
+			}
+			Expr::Unop(_, e) => {
+				e.accept_mut(v);
+			}
+			Expr::Insn(i) => i.accept_mut(v),
+			Expr::Const(_) => {}
+			Expr::Flag(x) => accept_val_mut(IAM::Flag(x), v),
+			Expr::Var(x) => accept_val_mut(IAM::Var(x), v),
+			Expr::Attr(x) => accept_val_mut(IAM::Attr(x), v),
+			Expr::CharAttr(x) => accept_val_mut(IAM::CharAttr(x), v),
+			Expr::Rand => {}
+			Expr::Global(x) => accept_val_mut(IAM::Global(x), v),
+		}
+	}
+}
+
+impl VisitMut for ED7Quest {
+	fn accept_mut(&mut self, v: &mut impl FnMut(IAM)) {
+		accept_val_mut(IAM::QuestId(&mut self.id), v);
+		accept_val_mut(IAM::TextTitle(&mut self.name), v);
+		accept_val_mut(IAM::TextTitle(&mut self.client), v);
+		accept_val_mut(IAM::Text(&mut self.desc), v);
+		for s in &mut self.steps {
+			accept_val_mut(IAM::Text(s), v);
+		}
+	}
+}
+
+fn accept_val_mut(mut e: IAM, v: &mut impl FnMut(IAM)) {
+	match &mut e { // Skip unspecific types
+		IAM::i16(_) |
+		IAM::i32(_) |
+		IAM::u8(_) |
+		IAM::u16(_) |
+		IAM::u32(_) |
+		IAM::String(_) => return,
+		_ => {}
+	}
+	match &mut e {
+		IAM::CharAttr(a) => {
+			accept_val_mut(IAM::CharId(&mut a.0), v)
+		}
+		IAM::Fork(a) => {
+			for i in a.iter_mut() {
+				i.accept_mut(v);
+			}
+		}
+		IAM::MandatoryMembers(a) => {
+			for i in a.iter_mut().flatten() {
+				accept_val_mut(IAM::Member(i), v);
+			}
+		}
+		IAM::OptionalMembers(a) => {
+			for i in a.iter_mut() {
+				accept_val_mut(IAM::Member(i), v);
+			}
+		}
+		IAM::Menu(a) => {
+			for i in a.iter_mut() {
+				accept_val_mut(IAM::MenuItem(i), v);
+			}
+		}
+		IAM::QuestList(a) => {
+			for i in a.iter_mut() {
+				accept_val_mut(IAM::QuestId(i), v);
+			}
+		}
+		_ => {}
+	}
+	v(e);
+}
