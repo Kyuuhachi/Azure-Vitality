64f38f8d56f3f0de5fbebc70007b02ac1f2396e1
Kyuuhachi <caagr98@gmail.com>
2022-12-25 12:37:07 +0100

    Extract translatable stuff from Text

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use std::path::{Path, PathBuf};
 use std::collections::HashMap;
 
+use regex::Regex;
 use themelios::gamedata::GameData;
 use themelios::scena;
 use themelios::scena::code::{Expr, FlatInsn, Insn, InsnArgMut as IAM};
 use themelios::scena::code::decompile::{TreeInsn, decompile, recompile};
 use themelios::scena::ed7::Scena;
 use themelios::tables::quest::{ED7Quest, self};
-use themelios::text::Text;
+use themelios::text::{Text, TextSegment};
 use themelios::types::{QuestId, Flag};
 use visit::VisitMut;
 
@@ -279,16 +280,98 @@ fn translate<T: Clone + VisitMut>(tl: &mut impl Translator, a: &T) -> T {
 	a.accept_mut(&mut |a| {
 		match a {
 			IAM::Text(a) => *a = translate_text(tl, a),
-			IAM::TextTitle(a) => *a = tl.translate(a),
-			IAM::MenuItem(a) => *a = tl.translate(a),
+			IAM::TextTitle(a) if !a.is_empty() => *a = tl.translate(a),
+			IAM::MenuItem(a) if !a.is_empty() => *a = tl.translate(a),
 			_ => {}
 		}
 	});
 	a
 }
 
+fn text2str(t: &Text) -> String {
+	let mut s = String::new();
+	for i in t.iter() {
+		match i {
+			TextSegment::String(v) => s.push_str(v),
+			TextSegment::Line => s.push('\n'),
+			TextSegment::Line2 => s.push('\r'),
+			TextSegment::Wait => s.push_str("{wait}"),
+			TextSegment::Page => s.push_str("{page}"),
+			TextSegment::Color(v) => s.push_str(&format!("{{color {v}}}")),
+			TextSegment::Item(v) => s.push_str(&format!("{{item {v}}}", v=v.0)),
+			TextSegment::Byte(v) => s.push_str(&format!("{{#{v:02X}}}")),
+		}
+	}
+	s
+}
+
+
+fn str2text(s: &str) -> Text {
+	lazy_static::lazy_static! {
+		static ref SEGMENT: Regex = Regex::new(r"(?x)
+			(?P<t>.*?)
+			(?:
+				(?P<line>)\n|
+				(?P<line2>)\r|
+				(?P<wait>)\{wait\}|
+				(?P<page>)\{page\}|
+				\{color\ (?P<color>\d+)\}|
+				\{item\ (?P<item>\d+)\}|
+				\{\#(?P<hex>[[:xdigit:]]{2})\}|
+				$
+			)
+		").unwrap();
+	}
+	let mut out = Text(Vec::new());
+	for c in SEGMENT.captures_iter(s) {
+		if let Some(t) = c.name("t") {
+			if !t.as_str().is_empty() {
+				out.push(TextSegment::String(t.as_str().to_owned()))
+			}
+		}
+		if c.name("line").is_some() {
+			out.push(TextSegment::Line)
+		}
+		if c.name("line2").is_some() {
+			out.push(TextSegment::Line2)
+		}
+		if c.name("wait").is_some() {
+			out.push(TextSegment::Wait)
+		}
+		if c.name("page").is_some() {
+			out.push(TextSegment::Page)
+		}
+		if let Some(c) = c.name("color") {
+			out.push(TextSegment::Color(c.as_str().parse().unwrap()))
+		}
+		if let Some(c) = c.name("item") {
+			out.push(TextSegment::Item(c.as_str().parse::<u16>().unwrap().into()))
+		}
+		if let Some(c) = c.name("hex") {
+			out.push(TextSegment::Byte(u8::from_str_radix(c.as_str(), 16).unwrap()))
+		}
+	}
+	out
+}
+
 fn translate_text(tl: &mut impl Translator, t: &Text) -> Text {
-	t.clone()
+	lazy_static::lazy_static! {
+		static ref CONTENT: Regex = Regex::new(r"(?xs)
+			^
+			((?:\{.*?\}|\#\d+[S])*)
+			((?:\#\d*[ABFPVWZ])*)
+			(.*?)
+			((?:\{wait\})?)
+			$
+		").unwrap();
+	}
+	let s = text2str(t);
+	assert_eq!(t, &str2text(&s));
+	let s2 = s.split("{page}").map(|p| {
+		let c = CONTENT.captures(p).unwrap();
+		format!("{}{}{}", &c[2], tl.translate(&format!("{}{}", &c[1], &c[3])), &c[4])
+	}).collect::<Vec<_>>().join("{page}");
+	str2text(&s2)
 }
 
 macro_rules! flag {
